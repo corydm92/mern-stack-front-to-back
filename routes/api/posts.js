95c6f1185cb153ca27d92a5c5e74ed7499001bfe
Cory Morrissey <contact@corymorrissey.com>
2020-09-28 12:12:52 -0700

    refactored to fetch user, pull name and avatar from that model, and create new post object using those values

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const Post = require('../../models/Post');
+const User = require('../../models/User');
 const { check, validationResult } = require('express-validator');
 
 // @route   GET api/posts
@@ -22,17 +23,23 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { text, name, avatar } = req.body;
-    const userId = req.user.id;
+    const user = await User.findById(req.user.id);
 
-    const payload = { text, name, avatar, user: userId };
+    const { text } = req.body;
+
+    const postOptions = {
+      text,
+      name: user.name,
+      avatar: user.avatar,
+      user: user.id,
+    };
 
     try {
-      const post = new Post(payload);
+      const post = new Post(postOptions);
 
       post.save();
 
-      res.status(200).json(payload);
+      res.status(200).json(post);
     } catch (err) {
       console.error(error.message);
       res.status(500).send('Server Error');
