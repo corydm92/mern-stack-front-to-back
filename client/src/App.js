dfd7876c75f5735d8a32e173aa275658fa37c8a7
Cory Morrissey <contact@corymorrissey.com>
2020-10-02 10:27:57 -0700

    added routing to landing page and nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => (
-  <React.Fragment>
-    <div className='App'>App</div>
-  </React.Fragment>
+  <Router>
+    <React.Fragment>
+      <Navbar />
+      <Route exact path='/' render={() => <Landing />} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' render={() => <Register />} />
+          <Route exact path='/login' render={() => <Login />} />
+        </Switch>
+      </section>
+    </React.Fragment>
+  </Router>
 );
 
 export default App;
