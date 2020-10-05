3d9092ed6c0b4c044ee157a0ffa4f25fb28b14a9
Cory Morrissey <contact@corymorrissey.com>
2020-10-05 10:35:38 -0700

    added action creator for setting alert

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,7 +1,9 @@
+import uuid from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
-export const setAlert = (alert) => {
-  return { type: SET_ALERT, payload: alert };
+export const setAlert = (msg, alertType) => (disaptch) => {
+  const id = uuid.v4();
+  disaptch({ type: SET_ALERT, payload: { msg, alertType, id } });
 };
 
 export const removeAlert = (alertId) => {
