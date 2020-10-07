0accc565e23b6844c45b84ab6cb8bd894293b5fb
Cory Morrissey <contact@corymorrissey.com>
2020-10-07 10:22:01 -0700

    added optional timeout paramater to setAlert

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,11 +1,11 @@
 import uuid from 'uuid/v4';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
-export const setAlert = (msg, alertType) => (dispatch) => {
+export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
   const id = uuid();
   dispatch({ type: SET_ALERT, payload: { msg, alertType, id } });
   // Having dispatch instead of a blanket return allows us to call multiple actions without needing an intermediary action creator
-  setTimeout(() => dispatch(removeAlert(id)), 5000);
+  setTimeout(() => dispatch(removeAlert(id)), timeout);
 };
 
 export const removeAlert = (alertId) => {
