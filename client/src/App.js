0cd36062605f15d1b0149e11d08d454c447714dd
Cory Morrissey <contact@corymorrissey.com>
2020-10-07 09:31:05 -0700

    created alert component, working with prop types library

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Alert from './components/layout/Alert';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -15,6 +16,7 @@ const App = () => (
         <Navbar />
         <Route exact path='/' render={() => <Landing />} />
         <section className='container'>
+          <Alert />
           <Switch>
             <Route exact path='/register' render={() => <Register />} />
             <Route exact path='/login' render={() => <Login />} />
--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = (props) => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -20,7 +21,7 @@ const Register = (props) => {
     e.preventDefault();
 
     if (password !== password2) {
-      props.setAlert('Passwords do not match', 'danger');
+      setAlert('Passwords do not match', 'danger');
     } else {
       console.log('Success');
     }
@@ -94,6 +95,10 @@ const Register = (props) => {
 
 const mapDispatchToProps = (dispatch, getState) => {
   return { setAlert: (msg, alertType) => dispatch(setAlert(msg, alertType)) };
+};
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
 };
 
 export default connect(null, mapDispatchToProps)(Register);
