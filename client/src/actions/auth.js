d2ae82bc0d0fc35b5d82a28c64d2bc5e68ff8a5f
Cory Morrissey <contact@corymorrissey.com>
2020-10-12 10:07:05 -0700

    filled out loadUser action creator

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,8 +6,25 @@ import {
   AUTH_ERROR,
 } from './types';
 import { setAlert } from './alert';
+import setAuthToken from '../utils/setAuthToken';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+// Load User
+
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get(`${BASE_URL}/api/auth`);
+
+    dispatch({ type: USER_LOADED, payload: res.data });
+  } catch (err) {
+    dispatch({ type: AUTH_ERROR });
+  }
+};
 
 // Register User
 export const register = (name, email, password) => async (dispatch) => {
