88de4def291cfb3b4874fefe054aa4ca0e570c94
Cory Morrissey <contact@corymorrissey.com>
2020-10-13 11:14:09 -0700

    refactor navbar per course work, seperated ul rendering into constants, used ternary operation to check if is loading and to render links based on user state

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -4,7 +4,31 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 
-const Navbar = ({ isAuthenticated, logout }) => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const guestLinks = (
+    <ul>
+      <li>
+        <a href='#!'>Developers</a>
+      </li>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </ul>
+  );
+
+  const authLinks = (
+    <ul>
+      <li>
+        <a onClick={logout} to='/#!'>
+          <i className='fas fa-sign-out-alt'></i>{' '}
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </ul>
+  );
   return (
     <nav className='navbar bg-dark'>
       <h1>
@@ -12,37 +36,18 @@ const Navbar = ({ isAuthenticated, logout }) => {
           <i className='fas fa-code'></i> DevConnector
         </Link>
       </h1>
-      <ul>
-        <li>
-          <a href='!#'>Developers</a>
-        </li>
-        {isAuthenticated ? (
-          <li>
-            <Link onClick={() => logout()} to='/'>
-              Logout
-            </Link>
-          </li>
-        ) : (
-          <>
-            <li>
-              <Link to='/register'>Register</Link>
-            </li>
-            <li>
-              <Link to='/login'>Login</Link>
-            </li>
-          </>
-        )}
-      </ul>
+
+      {!loading && isAuthenticated ? authLinks : guestLinks}
     </nav>
   );
 };
 
 const mapStateToProps = (state) => ({
-  isAuthenticated: state.auth.isAuthenticated,
+  auth: state.auth,
 });
 
 Navbar.propTypes = {
-  isAuthenticated: PropTypes.bool,
+  auth: PropTypes.object.isRequired,
   logout: PropTypes.func.isRequired,
 };
 
