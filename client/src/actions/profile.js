ee4df69b1274419ee990579b47c1ae21a6aa8228
Cory Morrissey <contact@corymorrissey.com>
2020-10-15 10:12:00 -0700

    profile action creator successfully dispatching from dashboard

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,15 +1,17 @@
 import { PROFILE_SUCCESS, PROFILE_FAIL } from './types';
+import axios from 'axios';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-export const profileSuccess = () => dispatch =>    {
+export const fetchSelfProfile = () => async (dispatch) => {
   try {
     const res = await axios.get(`${BASE_URL}/api/profile/me`);
 
-    console.log(res)
+    console.log(res);
+    console.log('here');
 
     dispatch({ type: PROFILE_SUCCESS, payload: res.data });
   } catch (err) {
     dispatch({ type: PROFILE_FAIL });
   }
-}
+};
--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchSelfProfile } from '../../actions/profile';
 
-const Dashboard = () => {
+const Dashboard = ({ fetchSelfProfile }) => {
+  useEffect(() => {
+    fetchSelfProfile();
+  }, []);
   return <div>Dashboard</div>;
 };
 
-export default Dashboard;
+const mapDispatchToProps = (dispatch) => ({
+  fetchSelfProfile: () => dispatch(fetchSelfProfile()),
+});
+
+export default connect(null, mapDispatchToProps)(Dashboard);
