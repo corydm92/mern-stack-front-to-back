a11100533a8db307e83d90be91a25666630c47ff
Cory Morrissey <contact@corymorrissey.com>
2020-10-15 10:50:15 -0700

    refactored Dashboard component to hold state as well as actions

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,16 +1,28 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchSelfProfile } from '../../actions/profile';
+import PropTypes from 'prop-types';
 
-const Dashboard = ({ fetchSelfProfile }) => {
+const Dashboard = ({ fetchSelfProfile, auth, profile }) => {
   useEffect(() => {
     fetchSelfProfile();
   }, []);
   return <div>Dashboard</div>;
 };
 
+Dashboard.propTypes = {
+  fetchSelfProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   fetchSelfProfile: () => dispatch(fetchSelfProfile()),
 });
 
-export default connect(null, mapDispatchToProps)(Dashboard);
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
